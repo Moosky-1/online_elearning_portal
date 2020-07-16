5b06eaff63d0ec28fff1c514e484411a05727bc2
Moosky-1 <m.navti@gmail.com>
2020-07-16 19:05:49 +0100

    Trying the social page

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'^about', views.about, name='about'),
 	url(r'^contact', views.contact, name='contact'),
+    url(r'^social', views.contact, name='social'),
+
 ]	
