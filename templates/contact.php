5411a5a5a6e5d557c6c91d30b3aa39aac0ec064c
Moosky-1 <m.navti@gmail.com>
2020-07-17 19:53:59 +0100

    editing to fit csrf

--- templates/contact.php
+++ templates/contact.php
@@ -1,23 +1,38 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$phone = $_POST['phone'];
+session_start();
 
-$emai_from = 'Remote Classroom Student Enquiry';
-$email_subject = 'New Message from student';
-$email_body = "Name: $name.\n",
-              "Email: $email.\n",
-              "Message: $message.\n",
-              "Phone: $phone.\n";
+if (empty($_SESSION['key']))
+    $_SESSION['key'] = bin2hex(random_bytes(32));
 
-$to = "wiylanyuyM@cardiff.ac.uk";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $email \r\n";
+$csrf = hash_hmac('sha256','this is some string: contact.php', $_SESSION['key'])
 
-mail($to,$email_subject,$email_body,$headers);
+if(isset($_POST['submit'])){
+  if (hash_equals($csrf, $_POST['csrf'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $phone = $_POST['phone'];
 
-header("Location: index.html");
+    $emai_from = 'Remote Classroom Student Enquiry';
+    $email_subject = 'New Message from student';
+    $email_body = "Name: $name.\n",
+                  "Email: $email.\n",
+                  "Message: $message.\n",
+                  "Phone: $phone.\n";
 
+    $to = "wiylanyuyM@cardiff.ac.uk";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+  }else {
+    echo "CSRF token Failed";
+  }
+
+}
 
  ?>
